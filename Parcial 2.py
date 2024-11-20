7978eead325fc3e0b613d81d7b614e0f234f4159
Darickson18 <dleiva@correo.unimet.edu.ve>
2024-11-20 08:30:20 -0400

    Add files via upload

---
+++ Parcial 2.py
@@ -0,0 +1,10 @@
+from  import Class
+
+Class Producto:
+def_init(self, nombre, precio, cantidad)
+self. nombre = nombre
+self . precio = precio
+Self .cantidad = cantidad
+
+class producto:
+    
---
+++ product.py
@@ -0,0 +1,23 @@
+from producto import 
+
+lass inventario:
+def_init_(self):
+    self .productos = ()
+
+
+
+    class inventario: 
+    def_init_(self)
+      self .productosproductos = ()
+
+def agregar_producto(self):
+    while true :
+        print(" Bienvenido al semejo  ")
+        nombre = input("por favor ingrese el nombre del producto")
+        precio = float("input("por favor elije que precio va a tener )")
+        cantidad = int (input(por favor indique cuantas unidades este producto tendra en su inventario ))
+        if cantidad < 0
+        print("ha ingresado un dato invalido, por favor intentelo de nuevo")
+    perece = input ("por favor elije que caracteristicas desea que tenga su producto /n2 . no percedero. /n1 ")
+    
+        
